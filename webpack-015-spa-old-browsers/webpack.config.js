4db9b536628a2298bb658afd1ab411d1e04691a5
asr <ajsoriar@gmail.com>
2019-07-02 23:43:00 +0200

    Use ejs template as index

--- webpack-015-spa-old-browsers/webpack.config.js
+++ webpack-015-spa-old-browsers/webpack.config.js
@@ -1,6 +1,12 @@
+const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
   module: {
     rules: [
       {
@@ -27,8 +33,11 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
+      title: 'Page title!',
+      content_title: 'Content title!',
+      //template: "./src/index.html",
+      //filename: "./index.html"
+      template: './src/index.template.ejs'
     }),
     new MiniCssExtractPlugin({
       filename: "[name].css",
