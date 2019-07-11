b47c3b06be8e5cd3bbc487e99e2a38169b26e43e
asr <ajsoriar@gmail.com>
2019-07-12 00:00:40 +0200

    Preact clock

--- webpack-016-spa-old-browsers-preact/src/index.js
+++ webpack-016-spa-old-browsers-preact/src/index.js
@@ -1,5 +1,7 @@
 import "./components/sideMenu/sideMenu";
+import Clock from './components/clock/clock.js';
 import preact from 'preact';
+import { render } from 'preact';
 
 // Tell Babel to transform JSX into preact.h() calls:
 /** @jsx preact.h */
@@ -25,33 +27,4 @@ Bye();
 App.init();
 
 const reactRootNode = document.getElementById("root");
-
-import { h, render, Component } from 'preact';
-
-class Clock extends Component {
-    constructor() {
-        super();
-        // set initial time:
-        this.state.time = Date.now();
-    }
-
-    componentDidMount() {
-        // update time every second
-        this.timer = setInterval(() => {
-            this.setState({ time: Date.now() });
-        }, 1000);
-    }
-
-    componentWillUnmount() {
-        // stop when not renderable
-        clearInterval(this.timer);
-    }
-
-    render(props, state) {
-        let time = new Date(state.time).toLocaleTimeString();
-        return <span>{ time }</span>;
-    }
-}
-
-// render an instance of Clock into <body>:
 render(<Clock />, reactRootNode);
